217adb525f62218f816af1f6f6dff11a7f5d2cf9
Ayebakuro <xplicitkuro@gmail.com>
2019-02-18 13:38:11 +0100

    inititated frontend

---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link className="navbar-brand" to="/">POSAC</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/end-of-day">End of Day Report</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/transaction-history">Transaction History</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
